bc0d55f5ed4c2e32b4e647b4260e8b7ebfe2311a
Luke Harris <lukeharris01170@gmail.com>
2020-06-14 19:31:35 +0100

    Add index buffer

--- examples/glfw/main.cc
+++ examples/glfw/main.cc
@@ -67,19 +67,30 @@ int main()
 
     std::cout << glGetString(GL_VERSION) << std::endl;
 
-    float positions[6] = {
+    float positions[] = {
+        -0.5f,  0.5f,
+         0.5f,  0.5f,
         -0.5f, -0.5f,
-         0.0f,  0.5f,
          0.5f, -0.5f
+    };
+
+    unsigned int indicies[] = {
+        2, 3, 1,
+        2, 1, 0
     };
 
     unsigned int buffer;
     glGenBuffers(1, &buffer);
     glBindBuffer(GL_ARRAY_BUFFER, buffer);
-    glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positions, GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, 6 * 2 * sizeof(float), positions, GL_STATIC_DRAW);
 
     glEnableVertexAttribArray(0);
     glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), 0);
+
+    unsigned int ibo;
+    glGenBuffers(1, &ibo);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, 6 * sizeof(unsigned int), indicies, GL_STATIC_DRAW);
 
     std::ifstream stream("engine/shaders/basic.glsl");
 
@@ -98,7 +109,7 @@ int main()
     {
         glClear(GL_COLOR_BUFFER_BIT);
 
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
 
         glfwSwapBuffers(window);
         glfwPollEvents();
